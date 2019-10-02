2cccaae0fb471669a8af17dff16ec284cda1f66f
Owen Jaques <owenjaques@uvic.ca>
2019-10-01 17:52:08 -0700

    got the string testing working, wierd bug when I create my own tests though always seem to have a \n char

--- RLE.c
+++ RLE.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-int main(int argc, int *argv[]){
-	if(argc < 3 || strcmp(argv[2], "e") != 0 && strcmp(argv[2], "d") != 0){
+#define STRING_MAX 41
+#define ZERO_ASCII 48
+#define NINE_ASCII 57
+#define A_ASCII 65
+#define Z_ASCII 90
+#define SPACE_ASCII 32
+#define TAB_ASCII 9
+
+int main(int argc, char *argv[]){
+	if(argc < 3 || argv[2][0] != 'e' && argv[2][0] != 'd'){
 		printf("Invalid Usage, expected: RLE{filename}[e|d]\n");
 		exit(4);
 	}
-	//should also check if there is no input file specified but wont the first error a few lines up be fired first??
+
+	if(argv[1][0] == '\0'){
+		printf("Error: No input file specified!\n");
+		exit(1);
+	}
+
 	FILE * input_file = fopen(argv[1], "r");
 
 	if(input_file == NULL){
@@ -15,6 +27,32 @@ int main(int argc, int *argv[]){
 		exit(2);
 	}
 
+	char string[STRING_MAX];
+	fgets(string, STRING_MAX, input_file);
+	
+	//checks string
+	int white_space_flag = 0;
+	int i;
+	//added the check for a new line because for some reason when I create my own tests they always end with a new line no clue why
+	for(i = 0; i < STRING_MAX, string[i] != '\0', string[i] != '\n'; i++){
+		//sees if the string characters are within the ascii ranges that are allowed and if there has been no white space so far
+		if(!(((string[i] >= ZERO_ASCII && string[i] <= NINE_ASCII) || (string[i] >= A_ASCII && string[i] <= Z_ASCII)) && white_space_flag == 0)){
+			if(string[i] == SPACE_ASCII || string[i] == TAB_ASCII){
+				white_space_flag = 1;
+			}
+			else {
+				printf("Error: Invalid format\n");
+				exit(3);
+			}	
+		}
+		//if the white space flag has been set this will signify the string containing something other than white space after white space
+		else {
+			if(white_space_flag){
+				printf("Error: Invalid format\n");
+				exit(3);
+			}
+		}
+	}
 	
 	fclose(input_file);
 		
